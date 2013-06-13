83e4bcc981baa1f0218f231f7cf8b5395290ffc6
Anton Kulinsky <ankulinsky@gmail.com>
2013-06-13 16:18:26 +0600

    init

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var serviceURL = "http://sacreddeer.herokuapp.com/deer/";
+
+$("#sacreddeerListPage").bind("pageinit", function(event) {
+	$("button").bind("click", function(e) {
+
+	});
+});
+
+function showGPS() {
+    if(navigator.geolocation) {
+        // This is the specific PhoneGap API call
+        navigator.geolocation.getCurrentPosition(function(p) {
+            // p is the object returned
+            alert('Latitude '+p.coords.latitude);
+            alert('Longitude '+p.coords.longitude);
+        }, function(error){
+            alert("Failed to get GPS location");
+        });
+    } else {
+        alert("Failed to get GPS working");
+    }
+}
