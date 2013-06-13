41f34d8f74fe6982ca6a2bacc89e15661286d3a9
Anton Kulinsky <ankulinsky@gmail.com>
2013-06-13 21:32:53 +0600

    добавил leaflet

--- js/script.js
+++ js/script.js
@@ -24,28 +24,3 @@ function showGPS() {
 function mapPosition(lat, lng){
     map.setView([lat, lng], map.getZoom());
 }
-
-function mapInit() {
-    var zoomCtrl = new L.Control.Zoom({
-        position: 'bottomright'
-    });
-
-    var attrCtrl = new L.Control.Attribution({
-        position: 'bottomright',
-        prefix: ''
-    });
-
-    map = L.map('map',{
-        zoomControl: false,
-        attributionControl: false,
-        dragging: true
-    }).setView([59.4061536911142, 56.80575370788574], 15);
-
-    map.addControl(zoomCtrl);
-    map.addControl(attrCtrl);
-    console.log(map);
-
-    L.tileLayer('http://{s}.tile.cloudmade.com/282e84ccee6343f9b7440ac84f31dbf6/997/256/{z}/{x}/{y}.png', {
-        maxZoom: 18
-    }).addTo(map);
-}
